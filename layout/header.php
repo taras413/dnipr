cc156e83c64654e48fc897cbf6d84b309580a249
Taras Holets <taras.holets@gmail.com>
2016-06-06 19:13:19 +0300

    Перша верстка

---
+++ layout/header.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+		
+		<title>Dnipro LLC</title>
+
+		<link href="css/normalize.css" rel="stylesheet">
+		<link href="css/style.css" rel="stylesheet">
+		<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet">
+		
+		<script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+		<script type="text/javascript" src="js/script.js"></script>
+	</head>
+	   <body>
+            <main id="content">
+                <header class="root">
+                    <div class="container">
+                        <ul class="clearfix">
+                            <header class="logo">
+                                <img src="images/logo.png" alt="">
+                                <img src="images/clook.png" alt="">
+                            </header>
+                            <header class="top">
+                                <nav class="primary">
+                                    <ul>
+                                        <li><a href="#"><img class="image-top" src="images/Kabinet.png">Кабінет</a></li>
+                                        <li><a href><img class="image-top" src="images/map.png">Трекінг</a></li>
+                                        <li><a href="#"><img class="image-top" src="images/calculator.png">Вартість</a></li>
+                                        <li><a href="#"><img class="image-top" src="images/help.png">Підтримка</a></li>
+                                    </ul>              
+                                </nav>
+                            </header>                        
+                            <header class="botton">
+                                <nav class="botton-nav">
+                                    <ul>
+                                        <li><a href="#">Сервіси <i class="fa fa-angle-down" aria-hidden="true"></i></a>
+                                            <ul>
+                                                <li><a href="#">Авіа Перевезення</a></li>
+                                                <li><a href="#">Морські Перевезення <i class="fa fa-angle-right" aria-hidden="true"></i></a></li>  
+                                                <li><a href="#">Перевезення по США <i class="fa fa-angle-right" aria-hidden="true"></i></a></li>
+                                                <li><a href="#">Складські послуги <i class="fa fa-angle-right" aria-hidden="true"></i></a></li>
+                                            </ul>
+                                        </li>                            
+                                        <li><a href="#"><i class="fa fa-shopping-cart" style="color: #a6ce39" aria-hidden="true"></i>  Amazon&amp;eBay</a></li>        <li><a href="#"><i class="fa fa-car" style="color: #a6ce39" aria-hidden="true"></i>  Авто-мото <i class="fa fa-angle-down" aria-hidden="true"></i></a>
+                                            <ul>
+                                                <li><a href="#">Доставка Автомобіліа</a></li>
+                                                <li><a href="#">Доставка Мотоциклів </a></li>
+                                                <li><a href="#">Доставка Лодок </a></li>
+                                                <li><a href="#">Перевезення авто по США </a></li>
+                                                <li><a href="#">Доставка Спец.Техніки </a></li>
+                                            </ul>
+                                        </li>
+                                        <li><a href="#">Блог</a></li>
+                                        <li><a href="#">Контакти</a></li>
+                                        <li><a href="#"><i class="fa fa-search" style="color: #a6ce39" aria-hidden="true"></i></a></li>
+                                        <li><a href="#" style="color: #0072bc">укр</a></li>
+                                        <li><a href="#" style="color: #0072bc">рус</a></li>
+                                        <li><a href="#" style="color: #0072bc">eng</a></li>
+
+                                    </ul>
+                                </nav>
+                        </ul>
+                    </div>
+                </header>
+            </main>
+
+
+
+
+
+
+
+	
+
+	
+
+
+
+
+
+
+
